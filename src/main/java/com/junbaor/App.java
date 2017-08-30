428cfc8d6fd516c28d357eb945dc9985c5263716
junbao <junbaor@qq.com>
2017-08-30 17:40:23 +0800

    add download echo bot

--- src/main/java/com/junbaor/App.java
+++ src/main/java/com/junbaor/App.java
@@ -1,5 +1,6 @@
 package com.junbaor;
 
+import com.junbaor.handler.EchoHandler;
 import com.junbaor.handler.FanfouHandler;
 import com.junbaor.handler.GetIdHandler;
 import com.junbaor.util.AppUtils;
@@ -27,6 +28,9 @@ public class App {
     @Autowired
     private GetIdHandler getIdHandler;
     @Autowired
+    private EchoHandler echoHandler;
+
+    @Autowired
     private AppUtils appUtils;
 
     public static void main(String[] args) {
@@ -41,6 +45,7 @@ public class App {
         try {
             telegramBotsApi.registerBot(fanfouHandler);
             telegramBotsApi.registerBot(getIdHandler);
+            telegramBotsApi.registerBot(echoHandler);
         } catch (TelegramApiException e) {
             log.error(e.getMessage(), e);
             appUtils.sendServerChan(e.getMessage(), ExceptionUtils.getStackTrace(e));
---
+++ src/main/java/com/junbaor/handler/EchoHandler.java
@@ -0,0 +1,104 @@
+package com.junbaor.handler;
+
+import com.junbaor.model.EchoResponsePo;
+import com.junbaor.util.AppUtils;
+import net.dongliu.requests.Requests;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.api.methods.send.SendAudio;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Component
+public class EchoHandler extends TelegramLongPollingBot {
+
+    private static final Logger log = LoggerFactory.getLogger(EchoHandler.class);
+
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    @Value("${echo.bot.token}")
+    private String botToken;
+
+    @Value("${echo.bot.name}")
+    private String botName;
+
+    public EchoHandler(@Autowired DefaultBotOptions options) {
+        super(options);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.getMessage() == null) {
+            return;
+        }
+        executorService.submit(new EchoDownload(update));
+    }
+
+    class EchoDownload implements Runnable {
+
+        private Update update;
+
+        EchoDownload(Update update) {
+            this.update = update;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Long chatId = update.getMessage().getChatId();
+                Integer messageId = update.getMessage().getMessageId();
+                String text = update.getMessage().getText();
+
+                if (text.indexOf("app-echo.com") < 0) {
+                    return;
+                }
+
+                String musicId = text.replaceAll("http.?://www.app-echo.com/#/sound/", "").replace("http.?://www.app-echo.com/sound/info\\?sound_id=", "");
+
+                String jsonString = Requests.get("http://www.app-echo.com/api/sound/info?id=" + musicId).send().readToText();
+                EchoResponsePo responsePo = AppUtils.gson.fromJson(jsonString, EchoResponsePo.class);
+                if (responsePo.getStatus() != 100) {
+                    return;
+                }
+
+                String fileName = responsePo.getInfo().getName();
+                String filePath = System.currentTimeMillis() + "-" + fileName;
+                System.out.println(filePath);
+
+                File file = new File(AppUtils.getFilePath() + filePath);
+                Requests.get(responsePo.getInfo().getSource()).send().writeToFile(file);
+
+                SendAudio document = new SendAudio();
+                document.setChatId(chatId);
+                document.setReplyToMessageId(messageId);
+                document.setTitle(fileName);
+                document.setPerformer(responsePo.getInfo().getSong_info().getAuthor().getName());
+                document.setNewAudio(file);
+                sendAudio(document);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
+        }
+    }
+
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+
+}
